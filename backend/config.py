45934e72c4484f2c6dff4da1fb746f4a9a3af824
GiomarMC <gmunozcu@unsa.edu.pe>
2025-06-27 09:49:40 -0500

    Se modifica la conexion de la base de datos, ahora se hace utilizando variables de entorno

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,13 @@
 from mongoengine import connect
+from dotenv import load_dotenv
+from pathlib import Path
+import os
 
 
 def init_db():
-    connect(
-        db="denuncias_db",
-        host=(
-            "mongodb://admin:password123@localhost:27017/"
-            "denuncias_db?authSource=admin"
-        )
-    )
+    env_path = Path(__file__).parent.parent / ".env"
+    load_dotenv(dotenv_path=env_path)
+    uri = os.getenv("MONGO_URI")
+    if not uri:
+        raise ValueError("MONGO_URI no esta definido en el archivo .env")
+    connect(host=uri)
