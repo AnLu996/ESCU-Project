9d80a7f81ea05dc795219d4941c0c354441bf092
GiomarMC <gmunozcu@unsa.edu.pe>
2025-06-28 22:40:16 -0500

    Se instancia los settings para las variables de entorno de las secret_key, se agrega a los cors las respectivas credenciales

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,25 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 from app.interfaces.http.auth_routes import auth_bp
+from app.config.settings import settings
+
+jwt = JWTManager()
 
 
 def create_app():
     app = Flask(__name__)
+
+    # Configuracion de JWT desde settings
+    app.config["JWT_SECRET_KEY"] = settings.JWT_SECRET_KEY
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = settings.JWT_ACCESS_TOKEN_EXPIRES
+    app.config["SECRET_KEY"] = settings.SECRET_KEY
+
+    # Extensiones
+    CORS(app, supports_credentials=True, origins=["http://localhost:5173"])
+    jwt.init_app(app)
+
+    # Blueprints
     app.register_blueprint(auth_bp)
+
     return app
