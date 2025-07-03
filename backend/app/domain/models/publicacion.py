ce6c15a04f11f523f8ddf2ad8d09e106b17aa27d
GiomarMC <gmunozcu@unsa.edu.pe>
2025-07-03 11:58:50 -0500

    Se crea modelo para publicacion

---
+++ backend/app/domain/models/publicacion.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import Optional
+
+
+@dataclass
+class Publicacion:
+    contenido: str
+    fecha_creacion: datetime
+    fecha_actualizacion: datetime
+    reacciones: dict
+    usuario: str
+    anonimo: bool
+    id: Optional[str] = field(default=None)
