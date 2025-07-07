537c185b8370beb5746eb6c0ecc6e11ee777d03f
GiomarMC <gmunozcu@unsa.edu.pe>
2025-07-07 17:37:07 -0500

    Se implementa los metodos obtener, editar, eliminar

--- backend/app/infrastructure/database/publicacion_repo_impl.py
+++ backend/app/infrastructure/database/publicacion_repo_impl.py
@@ -3,6 +3,8 @@ from app.infrastructure.database.publicacion_document import (
     Publicacion as PublicacionDocument
 )
 from app.domain.repositories.publicacion_repo import PublicacionRepository
+from mongoengine.errors import DoesNotExist
+from datetime import datetime
 
 
 class MongoPublicacionRepository(PublicacionRepository):
@@ -11,7 +13,6 @@ class MongoPublicacionRepository(PublicacionRepository):
             contenido=publicacion.contenido,
             fecha_creacion=publicacion.fecha_creacion,
             fecha_actualizacion=publicacion.fecha_actualizacion,
-            reacciones=publicacion.reacciones,
             usuario=publicacion.usuario,
             anonimo=publicacion.anonimo
         )
@@ -19,5 +20,53 @@ class MongoPublicacionRepository(PublicacionRepository):
         publicacion.id = str(publicacion_document.id)
         return publicacion
 
-    def listar(self):
-        return Publicacion.objects.order_by("-fecha_creacion")
+    def obtener(self) -> list[Publicacion]:
+        publicacion_document = PublicacionDocument.objects.order_by(
+            "-fecha_creacion"
+        )
+        publicaciones = []
+
+        for doc in publicacion_document:
+            if doc.anonimo:
+                usuario_nombre = "Anonimo"
+            else:
+                usuario_nombre = doc.usuario.alias
+            publicaciones.append(Publicacion(
+                contenido=doc.contenido,
+                fecha_creacion=doc.fecha_creacion,
+                fecha_actualizacion=doc.fecha_actualizacion,
+                usuario=str(usuario_nombre),
+                anonimo=doc.anonimo,
+                id=str(doc.id)
+            ))
+
+        return publicaciones
+
+    def editar(
+        self,
+        publicacion_id: str,
+        nuevo_contenido: str,
+        autor_alias: str
+    ) -> bool:
+        try:
+            publicacion = PublicacionDocument.objects.get(id=publicacion_id)
+            if publicacion.usuario.alias != autor_alias:
+                return False
+
+            publicacion.contenido = nuevo_contenido
+            publicacion.fecha_actualizacion = datetime.now()
+            publicacion.save()
+            return True
+        except DoesNotExist:
+            return False
+
+    def eliminar(self, publicacion_id: str, autor_alias: str) -> bool:
+        try:
+            publicacion = PublicacionDocument.objects.get(id=publicacion_id)
+            if publicacion.usuario.alias != autor_alias:
+                return False
+
+            publicacion.delete()
+            return True
+        except DoesNotExist:
+            return False
