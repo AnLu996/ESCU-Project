192f6ee1a040b00a7524dcc73f4bc74f3baf07a4
GiomarMC <gmunozcu@unsa.edu.pe>
2025-07-07 17:35:51 -0500

    Se elimina campo de reacciones

--- backend/app/infrastructure/database/publicacion_document.py
+++ backend/app/infrastructure/database/publicacion_document.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from mongoengine import (
-    Document, StringField, DateTimeField, DictField, ReferenceField,
+    Document, StringField, DateTimeField, ReferenceField,
     BooleanField
 )
 from app.infrastructure.database.user_document import UserDocument
@@ -10,11 +10,8 @@ class Publicacion(Document):
     contenido = StringField(required=True, max_length=1000)
     fecha_creacion = DateTimeField(default=datetime.now)
     fecha_actualizacion = DateTimeField(default=datetime.now)
-    reacciones = DictField(default=lambda: {
-        "me_gusta": 0, "abrazo": 0, "fuerza": 0
-    })
     usuario = ReferenceField(UserDocument, required=True)
-    anonimo = BooleanField(default=True)
+    anonimo = BooleanField(default=False)
 
     meta = {
         "collection": "publicaciones",
