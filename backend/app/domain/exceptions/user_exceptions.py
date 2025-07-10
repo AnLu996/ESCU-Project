6d24b0b04e59d24e4188e4088021d757219de40d
Sebastian Himan <shiman@unsa.edu.pe>
2025-07-10 13:10:00 -0500

    Se modifico el archivo user_exceptions.py en el backend en la carpeta exceptions para poder hacer las pruebas del login test

--- backend/app/domain/exceptions/user_exceptions.py
+++ backend/app/domain/exceptions/user_exceptions.py
@@ -1,2 +1,9 @@
 class AliasYaExiste(Exception):
     pass
+class InvalidUsernameError(Exception):
+    def __init__(self, message="Alias no válido"):
+        super().__init__(message)
+
+class InvalidPasswordError(Exception):
+    def __init__(self, message="Contraseña no válida"):
+        super().__init__(message)
