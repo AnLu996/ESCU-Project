4b7be8a3dfcadb519efa4a39f615b004f0fe7084
RushhMax <rzavalaga@unsa.edu.pe>
2025-07-14 13:05:20 -0500

    admi page

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from '../pages/HomePage';
 import DenunciaPage from "../pages/DenunciaPage";
+import AdminDashboard from "../pages/admin";
 
 export default function AuthRoutes() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/denuncia" element={<DenunciaPage />} />
+      <Route path="/admin" element={<AdminDashboard />} />
     </Routes>
   );
 } 
