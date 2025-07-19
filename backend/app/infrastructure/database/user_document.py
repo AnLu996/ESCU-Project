30a21170a3c4466294a50998624dca1b5e2f9fd7
GiomarMC <gmunozcu@unsa.edu.pe>
2025-07-19 18:45:09 -0500

    Se agrega el campo de rol y se especifica los roles a manejar en el documento

--- backend/app/infrastructure/database/user_document.py
+++ backend/app/infrastructure/database/user_document.py
@@ -4,5 +4,6 @@ from mongoengine import Document, StringField
 class UserDocument(Document):
     alias = StringField(required=True, unique=True)
     password_hash = StringField(required=True)
+    rol = StringField(default='user', choices=['user', 'admin'])
 
     meta = {'collection': 'users'}
